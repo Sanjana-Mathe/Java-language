a464061775c4ab118a15589d6ed4feadc89efc23
Sanjana-Mathe <sanjanamathe02@gmail.com>
2025-08-01 10:09:46 +0530

    Add files via upload

---
+++ reversednum.java
@@ -0,0 +1,11 @@
+public class reverseString{
+public static void reversednum(String arg[]) {
+    String originalString="hello java";
+    String reverseString="";
+    for(int i=OriginalString.length() -1;i>=0;i--)
+      reverseString=reverseString+originalString.charAt(i);
+}
+    System.out.println("originalString:"+ originalString);
+    System.out.println("reverseString:"+  reverseString);
+  }
+}
