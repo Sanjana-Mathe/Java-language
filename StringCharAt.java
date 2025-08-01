931e52458a0ad127b51e4c0186bcd0ba429722db
Sanjana-Mathe <sanjanamathe02@gmail.com>
2025-08-01 10:22:07 +0530

    Add files via upload

---
+++ StringCharAt.java
@@ -0,0 +1,7 @@
+public class StringCharAt{
+    public static void main(String[] args) {
+        String word = "Program";
+        char character = word.charAt(2);
+        System.out.println("Character at index 2: " + character);
+    }
+}
